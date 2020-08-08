ea5166071932285701eef08c21b285cd0baa0ca3
wardhakashif <wardhakashif570@gmail.com>
2020-08-08 14:14:50 -0700

    updated Dinner.js

--- src/Dinner.js
+++ src/Dinner.js
@@ -5,7 +5,7 @@ function Dinner(props) {
     return (
       <div>
        <h1>Today we serve {props.dishname}</h1>
-        <h1>Today we serve {props.sweet}</h1>
+        <h1>Today we serve special sweet {props.sweet}</h1>
       </div>
     );
   }
